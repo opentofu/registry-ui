1a0ceb34ac4c84c1e57c973beb51036be15c9db3
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2024-08-21 16:00:08 +0200

    Convert md to html and render that directly
    Signed-off-by: Damian Stasik <920747+damianstasik@users.noreply.github.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./query";
 import { router } from "./router";
 import { AnnouncementBar } from "./components/AnnouncementBar";
-import announcement from "../announcement.md?raw";
+import announcement from "../announcement.md";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
