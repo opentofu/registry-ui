35efd4c4289ebac94566ba5b96040ddb43ce0523
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2024-08-22 14:59:35 +0200

    Make header static (#19)
    Signed-off-by: Damian Stasik <920747+damianstasik@users.noreply.github.com>

--- frontend/src/components/SidebarPanel/index.tsx
+++ frontend/src/components/SidebarPanel/index.tsx
@@ -10,7 +10,7 @@ export function SidebarPanel({ children, className }: SidebarPanelProps) {
   return (
     <aside
       className={clsx(
-        "sticky top-20 flex max-h-[calc(100vh-theme(height.20)-1px)] w-1/5 min-w-80 shrink-0 flex-col overflow-y-auto [scrollbar-width:thin]",
+        "sticky top-0 flex max-h-screen w-1/5 min-w-80 shrink-0 flex-col overflow-y-auto [scrollbar-width:thin]",
         className,
       )}
     >
