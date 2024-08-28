c1197a19078656d97f61250315fa2c4c3194c113
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2024-08-28 18:29:24 +0200

    Always show Examples and Submodules blocks (#77)
    Signed-off-by: Damian Stasik <920747+damianstasik@users.noreply.github.com>

--- frontend/src/routes/Module/components/ExamplesSidebarBlock.tsx
+++ frontend/src/routes/Module/components/ExamplesSidebarBlock.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { getModuleVersionDataQuery } from "../query";
 import { useState } from "react";
 import { useModuleParams } from "../hooks/useModuleParams";
+import { Paragraph } from "@/components/Paragraph";
 
 export function ModuleExamplesSidebarBlock() {
   const [expanded, setExpanded] = useState(false);
@@ -16,38 +17,39 @@ export function ModuleExamplesSidebarBlock() {
 
   const examples = Object.keys(data.examples);
 
-  if (examples.length === 0) {
-    return null;
-  }
-
   const visibleExamples = expanded ? examples : examples.slice(0, 5);
 
   return (
     <SidebarBlock title="Examples">
-      <ul className="mt-4 flex flex-col gap-4">
-        {visibleExamples.map((example) => (
-          <li key={example}>
-            <Link
-              to={`example/${example}`}
-              className="text-inherit underline underline-offset-2"
-            >
-              {example}
-            </Link>
-          </li>
-        ))}
+      {examples.length === 0 && (
+        <Paragraph>This module does not have any examples.</Paragraph>
+      )}
+      {examples.length > 0 && (
+        <ul className="mt-4 flex flex-col gap-4">
+          {visibleExamples.map((example) => (
+            <li key={example}>
+              <Link
+                to={`example/${example}`}
+                className="text-inherit underline underline-offset-2"
+              >
+                {example}
+              </Link>
+            </li>
+          ))}
 
-        {examples.length > 5 && (
-          <li>
-            <button
-              type="button"
-              onClick={() => setExpanded(!expanded)}
-              className="text-gray-700 underline underline-offset-2 dark:text-gray-300"
-            >
-              {expanded ? "Show less" : "Show all examples"}
-            </button>
-          </li>
-        )}
-      </ul>
+          {examples.length > 5 && (
+            <li>
+              <button
+                type="button"
+                onClick={() => setExpanded(!expanded)}
+                className="text-gray-700 underline underline-offset-2 dark:text-gray-300"
+              >
+                {expanded ? "Show less" : "Show all examples"}
+              </button>
+            </li>
+          )}
+        </ul>
+      )}
     </SidebarBlock>
   );
 }
