c16852a4c3db01cd770c201a85d359eb0461bd0e
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2024-08-30 16:05:42 +0200

    Adjust colors
    Signed-off-by: Damian Stasik <920747+damianstasik@users.noreply.github.com>

--- frontend/src/routes/Module/components/SchemaError.tsx
+++ frontend/src/routes/Module/components/SchemaError.tsx
@@ -3,10 +3,10 @@ import { warning } from "@/icons/warning";
 
 export function ModuleSchemaError() {
   return (
-    <div className="flex items-center gap-3 bg-brand-500/50 px-4 py-4 dark:bg-brand-800">
+    <div className="flex items-center gap-3 bg-brand-150 px-4 py-4 dark:bg-brand-800">
       <Icon
         path={warning}
-        className="size-em text-brand-800 dark:text-brand-600"
+        className="size-em text-brand-700 dark:text-brand-600"
       />
       We were unable to parse the schema for this module.
     </div>
