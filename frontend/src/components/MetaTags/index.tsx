9bc22780cc59d1e701196f980ce44a3d815223d5
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2024-09-04 09:31:01 +0200

    Use the same title in all tags
    Signed-off-by: Damian Stasik <920747+damianstasik@users.noreply.github.com>

--- frontend/src/components/MetaTags/index.tsx
+++ frontend/src/components/MetaTags/index.tsx
@@ -13,8 +13,8 @@ export function MetaTags({ title, description }: MetaTagsProps) {
   return (
     <Helmet>
       <title>{siteTitle}</title>
-      <meta property="og:title" content={title} />
-      <meta name="twitter:title" content={title} />
+      <meta property="og:title" content={siteTitle} />
+      <meta name="twitter:title" content={siteTitle} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
       {description && <meta name="twitter:description" content={description} />}
