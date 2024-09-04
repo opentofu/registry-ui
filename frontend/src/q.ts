0c84b9f78afc71efbefa5a8a999d42c7fab10096
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2024-09-04 13:49:46 +0200

    Increase search debounce delay
    Signed-off-by: Damian Stasik <920747+damianstasik@users.noreply.github.com>

--- frontend/src/q.ts
+++ frontend/src/q.ts
@@ -13,15 +13,7 @@ export const getSearchQuery = (query: string) =>
     queryFn:
       query.length > 0
         ? async ({ signal }) => {
-            // This is the lazy man's debounce, by waiting 100ms before making the request
-            // and utilizing the signal to cancel the request if the query changes
-            // we get a debounce effect
-            await (() => new Promise((resolve) => setTimeout(resolve, 100)))();
-            if (signal.aborted) {
-              return;
-            }
-
-            return await api(`search?q=${encodeURIComponent(query)}`, {
+            return api(`search?q=${encodeURIComponent(query)}`, {
               signal,
             }).json<definitions["SearchResultItem"][]>();
           }
