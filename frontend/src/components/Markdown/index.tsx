1ba9e711bd277bd76438a1f7b6a37b49a4048f9a
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2024-09-06 19:58:21 +0200

    Use existing MD pipeline and render the output to HTML at build-time
    Signed-off-by: Damian Stasik <920747+damianstasik@users.noreply.github.com>

--- frontend/src/components/Markdown/index.tsx
+++ frontend/src/components/Markdown/index.tsx
@@ -1,74 +1,12 @@
 import { useMemo } from "react";
-import * as prod from "react/jsx-runtime";
-
-import remarkFrontmatter from "remark-frontmatter";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import rehypeSlug from "rehype-slug";
-import remarkGfm from "remark-gfm";
-import rehypeReact, { Options } from "rehype-react";
-import rehypeSanitize from "rehype-sanitize";
-import rehypeRaw from "rehype-raw";
-import { unified } from "unified";
-import { MarkdownH1 } from "./H1";
-import { MarkdownP } from "./P";
-import { MarkdownPre } from "./Pre";
-import { MarkdownH2 } from "./H2";
-import { MarkdownH3 } from "./H3";
-import { MarkdownCode } from "./Code";
-import { MarkdownUl } from "./Ul";
-import { MarkdownLi } from "./Li";
-import { MarkdownA } from "./A";
-import { MarkdownTable } from "./Table";
-import { MarkdownTd } from "./Td";
-import { MarkdownTh } from "./Th";
-import { MarkdownImg } from "./Img";
-import { MarkdownOl } from "./Ol";
-import { MarkdownHr } from "./Hr";
-
-const production: Options = {
-  development: false,
-  Fragment: prod.Fragment,
-  jsx: prod.jsx,
-  jsxs: prod.jsxs,
-  components: {
-    a: MarkdownA,
-    ul: MarkdownUl,
-    li: MarkdownLi,
-    h1: MarkdownH1,
-    h2: MarkdownH2,
-    h3: MarkdownH3,
-    p: MarkdownP,
-    code: MarkdownCode,
-    pre: MarkdownPre,
-    table: MarkdownTable,
-    td: MarkdownTd,
-    th: MarkdownTh,
-    img: MarkdownImg,
-    ol: MarkdownOl,
-    hr: MarkdownHr,
-  },
-};
+import { processor } from "./processor";
 
 interface MarkdownProps {
   text: string;
 }
 
 export function Markdown({ text }: MarkdownProps) {
-  const { result } = useMemo(
-    () =>
-      unified()
-        .use(remarkParse)
-        .use(remarkFrontmatter)
-        .use(remarkGfm)
-        .use(remarkRehype, { allowDangerousHtml: true }) // This is okay to use dangerous html because we are sanitizing later on in the pipeline
-        .use(rehypeRaw)
-        .use(rehypeSanitize)
-        .use(rehypeSlug)
-        .use(rehypeReact, production)
-        .processSync(text),
-    [text],
-  );
+  const { result } = useMemo(() => processor.processSync(text), [text]);
 
   return result;
 }
