adf4b81d9d7eca42d763804a3a93d1f60e6cdf8f
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2024-09-10 13:11:13 +0200

    Fix access to provider data in error boundary (#213)
    Signed-off-by: Damian Stasik <920747+damianstasik@users.noreply.github.com>

--- frontend/src/routes/Provider/components/Error.tsx
+++ frontend/src/routes/Provider/components/Error.tsx
@@ -2,15 +2,23 @@ import { NotFoundPageError } from "@/utils/errors";
 import { Navigate, useLocation, useRouteError } from "react-router-dom";
 import { useProviderParams } from "../hooks/useProviderParams";
 
+function ProviderVersionRedirect() {
+  const params = useProviderParams();
+
+  return (
+    <Navigate
+      to={`/provider/${params.namespace}/${params.provider}/latest`}
+      replace
+    />
+  );
+}
+
 export function ProviderError() {
   const routeError = useRouteError() as Error;
   const location = useLocation();
-  const { namespace, provider, version } = useProviderParams();
 
   if (routeError instanceof NotFoundPageError && location.state?.fromVersion) {
-    return (
-      <Navigate to={`/provider/${namespace}/${provider}/${version}`} replace />
-    );
+    return <ProviderVersionRedirect />;
   }
 
   throw routeError;
