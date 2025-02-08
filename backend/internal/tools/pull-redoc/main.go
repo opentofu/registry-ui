af3e61289e76bcd96cbda3cb9ef00532bf7642ab
Sekiranda <sekirandahamza@gmail.com>
2025-02-08 12:25:01 +0300

    Download license file and copy downloads to tmp files
    Signed-off-by: Sekiranda <sekirandahamza@gmail.com>

--- backend/internal/tools/pull-redoc/main.go
+++ backend/internal/tools/pull-redoc/main.go
@@ -15,27 +15,58 @@ func main() {
 	flag.Parse()
 
 	// Pull Redoc script from Redocly CDN
-
 	redocCDNURL := fmt.Sprintf("https://cdn.redoc.ly/redoc/%s/bundles/redoc.standalone.js", version)
 
 	redocFileName := "redoc.standalone.js"
-	redocFile, err := os.Create(fmt.Sprintf("server/%s", redocFileName))
+	redocFile, err := os.Create("server/redoc.tmp")
 	if err != nil {
-		log.Print(err)
-		os.Exit(1)
+		log.Fatalf("err creating ReDoc script tmp file: %v", err)
 	}
 	defer redocFile.Close()
 
 	resp, err := http.Get(redocCDNURL)
-	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		err = fmt.Errorf("err downloading ReDoc script file: %+v\n", resp)
-		log.Print(err)
-		os.Exit(1)
+	if err != nil {
+		log.Fatalf("err downloading ReDoc script file: %v", err)
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		err = fmt.Errorf("err downloading ReDoc script file: %+v\n", resp)
+		log.Fatal(err)
+	}
 
 	if _, err = io.Copy(redocFile, resp.Body); err != nil {
-		log.Print(err)
-		os.Exit(1)
+		log.Fatal(err)
+	}
+
+	err = os.Rename("server/redoc.tmp", fmt.Sprintf("server/%s", redocFileName))
+	if err != nil {
+		log.Fatalf("Error renaming ReDoc script file: %v", err)
+	}
+
+	// Pull ReDoc LICENSE file
+	licenseFileURL := "https://raw.githubusercontent.com/Redocly/redoc/refs/heads/main/LICENSE"
+
+	licenseFile, err := os.Create("server/license.tmp")
+	if err != nil {
+		log.Fatalf("err creating ReDoc license tmp file: %v", err)
+	}
+	resp1, err := http.Get(licenseFileURL)
+	if err != nil {
+		log.Fatalf("err downloading ReDoc license file: %v", err)
+	}
+
+	defer resp1.Body.Close()
+	if resp1.StatusCode < 200 || resp1.StatusCode > 299 {
+		err = fmt.Errorf("err downloading ReDoc license file: %+v\n", resp1)
+		log.Fatal(err)
+	}
+
+	if _, err = io.Copy(licenseFile, resp1.Body); err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.Rename("server/license.tmp", "server/redoc.standalone.js.LICENSE.txt")
+	if err != nil {
+		log.Fatalf("Error renaming ReDoc license file: %v", err)
 	}
 }
