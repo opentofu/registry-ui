5ce5372dfd9bc742902e3bcba99604412b1d7e60
Christian Mesh <christianmesh1@gmail.com>
2025-03-06 09:29:10 -0500

    Remove search.ndjson from the public API docs (#312)
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

--- backend/internal/search/searchstorage/openapi.go
+++ backend/internal/search/searchstorage/openapi.go
@@ -1,20 +1,5 @@
 package searchstorage
 
-// GetSearchIndex returns a newline-delimited search index suitable for insertion into a database. The records are not
-// guaranteed to be in order.
-//
-// swagger:operation GET /registry/docs/search.ndjson Search GetSearchIndex
-// ---
-// produces:
-// - application/x-ndjson
-// responses:
-//   '200':
-//     description: A newline-delimited search index suitable for insertion into a database. The records are not guaranteed to be in order. Each item is a GeneratedIndexItem.
-//     schema:
-//       type: array
-//       items:
-//         $ref: '#/definitions/GeneratedIndexItem'
-
 // SearchResultItem describes a single search result item.
 //
 // swagger:model SearchResultItem
