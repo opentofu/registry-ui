3f46c9880d145de33cfbde317602a2d696ec4fab
Alex Jurkiewicz <alex@jurkiewi.cz>
2025-05-10 00:30:39 +0800

    Improve module input formatting (#322)
    Signed-off-by: Alex Jurkiewicz <alex@jurkiewi.cz>

--- frontend/src/components/ModuleInput/index.tsx
+++ frontend/src/components/ModuleInput/index.tsx
@@ -1,4 +1,5 @@
 import { HeadingLink } from "../HeadingLink";
+import { Markdown } from "../Markdown";
 import { Paragraph } from "../Paragraph";
 
 interface ModuleInputProps {
@@ -17,16 +18,18 @@ export function ModuleInput({
   const showDefaultValue = defaultValue !== undefined;
   return (
     <li>
-      <h4 id={name} className="group scroll-mt-5">
+      <h4 id={name} className="group scroll-mt-5 font-bold">
         {name}{" "}
         <code className="text-mono text-sm text-purple-700 dark:text-purple-300">
           ({type})
         </code>
         <HeadingLink id={name} label={`${name} input`} />
       </h4>
-      <Paragraph className="mt-1">{description}</Paragraph>
+      <Paragraph className="ml-4 mt-1">
+        <Markdown text={description} />
+      </Paragraph>
       {showDefaultValue && (
-        <Paragraph className="mt-2">
+        <Paragraph className="ml-4 mt-2">
           Default value:{" "}
           <code className="text-mono break-words text-sm text-purple-700 dark:text-purple-300">
             {JSON.stringify(defaultValue)}
