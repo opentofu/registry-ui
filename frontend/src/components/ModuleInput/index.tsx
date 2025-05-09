f0d9f610fb93f4200707f68c7b37e5e149fc2b7a
Alex Jurkiewicz <alex@jurkiewi.cz>
2025-05-09 12:18:52 +0800

    Improve module input formatting
    * Format input names as bold, so they stand out
    * Indent input description & defaults for a visual hierarchy
    * Render markdown in input descriptions
    
    Signed-off-by: Alex Jurkiewicz <alex@jurkiewi.cz>

--- frontend/src/components/ModuleInput/index.tsx
+++ frontend/src/components/ModuleInput/index.tsx
@@ -1,4 +1,5 @@
 import { HeadingLink } from "../HeadingLink";
+import { Markdown } from "../Markdown";
 import { Paragraph } from "../Paragraph";
 
 interface ModuleInputProps {
@@ -16,16 +17,16 @@ export function ModuleInput({
 }: ModuleInputProps) {
   return (
     <li>
-      <h4 id={name} className="group scroll-mt-5">
+      <h4 id={name} className="group scroll-mt-5 font-bold">
         {name}{" "}
         <code className="text-mono text-sm text-purple-700 dark:text-purple-300">
           ({type})
         </code>
         <HeadingLink id={name} label={`${name} input`} />
       </h4>
-      <Paragraph className="mt-1">{description}</Paragraph>
+      <Paragraph className="mt-1 ml-4"><Markdown text={description} /></Paragraph>
       {!!defaultValue && (
-        <Paragraph className="mt-2">
+        <Paragraph className="mt-2 ml-4">
           Default value:{" "}
           <code className="text-mono break-words text-sm text-purple-700 dark:text-purple-300">
             {JSON.stringify(defaultValue)}
