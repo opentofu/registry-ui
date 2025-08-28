3acb2750b56d48a76f3a9fb9559216f593510250
James Humphries <james@james-humphries.co.uk>
2025-08-28 12:23:57 +0100

    use info icon that was existing
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- frontend/src/routes/Home/components/WelcomeContent.tsx
+++ frontend/src/routes/Home/components/WelcomeContent.tsx
@@ -1,4 +1,6 @@
 import { Paragraph } from "@/components/Paragraph";
+import { Icon } from "@/components/Icon";
+import { info } from "@/icons/info";
 import Logo from "@/logo.svg?react";
 
 export function WelcomeContent() {
@@ -52,19 +54,10 @@ export function WelcomeContent() {
           >
             <div className="flex items-start gap-3">
               <div className="flex-shrink-0">
-                <svg
+                <Icon
+                  path={info}
                   className="h-5 w-5 text-sky-600 dark:text-sky-400 mt-0.5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
+                />
               </div>
               <div className="flex-1">
                 <h4 className="mb-1 text-sm font-semibold text-sky-900 dark:text-sky-100">
