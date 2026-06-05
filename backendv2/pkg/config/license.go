e8465c5851e4eee70a78f0cb937cbd4a8ee233eb
James Humphries <James@james-humphries.co.uk>
2026-06-05 12:24:16 +0100

    Set default licenses for backendv2 (#379)
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- backendv2/pkg/config/license.go
+++ backendv2/pkg/config/license.go
@@ -1,7 +1,5 @@
 package config
 
-import "fmt"
-
 type LicenseConfig struct {
 	CompatibleLicenses []string `koanf:"compatiblelicenses"`
 
@@ -20,10 +18,27 @@ type LicenseConfig struct {
 	ConfidenceOverrideThreshold float32 `koanf:"confidenceoverridethreshold"`
 }
 
+// defaultCompatibleLicenses is the set of SPDX licenses considered compatible
+// when no list is provided via config file or environment. Please keep in sync with
+// config.example.yaml.
+var defaultCompatibleLicenses = []string{
+	"AFL-1.1", "AFL-1.2", "AFL-2.0", "AFL-2.1", "AFL-3.0",
+	"Apache-1.1", "Apache-2.0",
+	"Artistic-1.0", "Artistic-1.0-Perl", "Artistic-1.0-cl8", "Artistic-2.0",
+	"0BSD", "BSD-1-Clause", "BSD-2-Clause", "BSD-2-Clause-Patent",
+	"BSD-3-Clause", "BSD-3-Clause-LBNL",
+	"CDDL-1.0",
+	"EPL-1.0", "EPL-2.0",
+	"ICU", "ISC",
+	"MIT", "MIT-0", "MIT-Modern-Variant", "MIT-feh",
+	"MPL-1.0", "MPL-1.1", "MPL-2.0", "MPL-2.0-no-copyleft-exception",
+	"Unlicense", "Xnet", "Zlib",
+}
+
 func (c *LicenseConfig) Validate() error {
-	// compatible licenses shouldn't be empty
+	// Fall back to the built-in default list when none is provided.
 	if len(c.CompatibleLicenses) == 0 {
-		return fmt.Errorf("compatible license list was empty or not provided")
+		c.CompatibleLicenses = defaultCompatibleLicenses
 	}
 
 	if c.ConfidenceThreshold == 0.0 {
